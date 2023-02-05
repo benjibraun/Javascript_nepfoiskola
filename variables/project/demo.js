e8bd9479abe85ca2ed47fa8c8cde670216d0bd97
benjibraun <benjibraun@gmail.com>
2023-02-05 14:26:21 +0100

    init

---
+++ variables/project/demo.js
@@ -0,0 +1,31 @@
+
+
+
+
+/*
+const myElement = document.getElementById("demo");
+myElement.style.color = "red";
+*/
+var x = 0;
+function update()
+{
+	
+}
+function add() 
+{
+	x = x + Number(document.getElementById("in").value);
+	document.getElementById("out").innerHTML = "output:" + x;
+	console.log(x);
+}
+function subtract() 
+{
+	
+}
+function multiply() 
+{
+	
+}
+function divide() 
+{
+	
+}
