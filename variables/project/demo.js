12e2563c2290ed1105cbb94c0b8dfc666dfd5725
benjibraun <benjibraun@gmail.com>
2023-02-18 12:57:24 +0100

    Computer says no!

--- variables/project/demo.js
+++ variables/project/demo.js
@@ -1,22 +1,19 @@
 var x = 0;
+var error = "no error yet";
 const pi = 3.14;
 
-var barna;
 
 function update()
 {
-	x = Number(document.getElementById("in").value);
+	x = Number(document.getElementById("in").value); //
 	document.getElementById("out").innerHTML = x;
 }
 function add() 
-{
-	
-	if (x == document.getElementById("in").value){
-		
+{	
 	x = x + Number(document.getElementById("in").value);
 	document.getElementById("out").innerHTML = "=" + x;
 	console.log(x);
-	}
+
 }
 function subtract() 
 {
@@ -31,8 +28,17 @@ function multiply()
 	console.log(x);
 }
 function divide() 
-{
-	x = x / Number(document.getElementById("in").value);
-	document.getElementById("out").innerHTML = "=" + x;
-	console.log(x);	
+{	
+	var input = Number(document.getElementById("in").value);
+	if (input != 0)
+	{
+		x = x / input;
+		document.getElementById("out").innerHTML = "=" + x;
+		console.log(x);
+	}
+	else{
+		error = "Computer says no!"
+		document.getElementById("out").innerHTML = error;
+		console.log(error);	
+	}
 }
