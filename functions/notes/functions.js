f0bf544e4aba56eb0e63ab71eec8eb2ff5cd4d94
benjibraun <benjibraun@gmail.com>
2023-02-19 19:14:36 +0100

    functions

---
+++ functions/notes/functions.js
@@ -0,0 +1,39 @@
+/* 
+variables -> https://www.w3schools.com/js/js_variables.asp
+data types -> https://www.w3schools.com/js/js_datatypes.asp
+*/
+
+//				-declaration and definition-     
+//declaration (technically undefined)
+
+
+var x; 
+let y;
+	z;
+const pi = 3.14;
+
+
+//assign (aka definition) 
+x = 23;
+y = "text"
+z = x + pi;
+
+//data types
+/*
+var a = 23; -> 0000 0011
+				   | t | e | x | t
+var b = "text"; -> 	116 101 120 116 
+				   |   t   |   e   |   x   |  t 
+				->	1110100 1100101 1111000 1110100				
+*/
+var a = 420; //Number (aka int)
+var b = "text"; //String
+var c = true; // Boolean
+var d = 1000000000000066600000000000001; //Bigint
+var e;	//undefined
+//null
+//Symbol
+//Object
+
+
+
